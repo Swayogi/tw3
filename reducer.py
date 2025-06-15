121e234499070ed3958a3142ac87656121dc5cd2
Swayogi <mrswayogi@gmail.com>
2025-06-15 17:52:13 +0530

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,1 +1,10 @@
-safdl
+#!/usr/bin/env python  
+import sys  
+gender_age = {}  
+for line in sys.stdin:  
+    gender, age = line.strip().split('\t')  
+    gender_age.setdefault(gender, []).append(int(age))  
+
+for gender in gender_age:  
+    ave_age = sum(gender_age[gender]) * 1.0 / len(gender_age[gender])  
+    print '%s\t%s' % (gender, ave_age)
