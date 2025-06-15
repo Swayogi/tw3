d8e27c1d050af315746508b0a1024b4d4e51a13a
Swayogi <mrswayogi@gmail.com>
2025-06-15 17:51:35 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,1 +1,8 @@
-adsf
+#!/usr/bin/env python  
+import sys  
+for line in sys.stdin:  
+    line = line.strip().split(",")  
+    if len(line) >= 2:  
+        gender = line[1]  
+        age = line[2]  
+        print '%s\t%s' % (gender, age)
